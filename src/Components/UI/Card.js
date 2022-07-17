6f66b04d1250f79a9e457ac2cdc510d8a80bf251
Steven P Meehan <steven@stevenmeehan.com>
2022-07-17 18:45:51 -0400

    removed the alterative options for car coloring

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -9,9 +9,7 @@ const Card = (props) => {
 
     const cardHover = `${props.cardHover ? classes.cardHover : ''}`;
 
-    const cardColoring = `${props.cardColor === 'light' ? classes.light : 
-                            props.cardColor === 'medium' ? classes.medium : 
-                            props.cardColor === 'dark' ? classes.dark : ''}`;
+    const cardColoring = `${props.cardColor === 'light' ? classes.light : ''}`;
 
     const cardClasses = `${props.cardClasses ? props.cardClasses : ''} ${classes.card} ${cardColoring} ${cardHover} ${cardRounded}`;
     
