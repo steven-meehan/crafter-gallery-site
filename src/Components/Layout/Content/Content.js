22d933e00595d35dbae47d6a32723696ecf002da
Steven P Meehan <steven@stevenmeehan.com>
2022-07-24 01:09:33 -0400

    updated the content component to implement lazy loading based on the top level routes

--- src/Components/Layout/Content/Content.js
+++ src/Components/Layout/Content/Content.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-
-import Home from '../../../Pages/Home';
-import Gallery from '../../../Pages/Gallery';
-import NotFound from '../../../Pages/NotFound';
 
 import classes from './Content.module.css';
 import Card from '../../UI/Card';
+import Spinner from '../../UI/Spinner';
 
 import data from '../../../Configs/TopLevelRoutes.json';
+
+const Home = React.lazy(() => import('../../../Pages/Home'));
+const Gallery = React.lazy(() => import('../../../Pages/Gallery'));
+const NotFound = React.lazy(() => import('../../../Pages/NotFound'));
 
 const Content = (props) => {
     const contentClasses = `${props.contentClasses ? props.contentClasses : ''} ${classes.content}`;
@@ -19,27 +20,42 @@ const Content = (props) => {
                 cardClasses={contentClasses}
                 cardRounded={true}
                 cardColor={`light`} >
-                <Switch>
-                    {data.map((route, index) => {
-                        return route.redirect.behavior && route.exact ? 
-                            <Route key={`main-route-${index}`} path={route.path} exact >
-                                <Redirect to={route.redirect.path} />
-                            </Route> : route.redirect.behavior && !route.exact ? 
-                            <Route key={`main-route-${index}`} path={route.path} >
-                                <Redirect to={route.redirect.path} />
-                            </Route> : route.component === "Home" ?
-                            <Route key={`main-route-${index}`} path={route.path} exact >
-                                <Home />
-                            </Route> : route.component === "Gallery" ?
-                            <Route key={`main-route-${index}`} path={route.path} >
-                                <Gallery />
-                            </Route> : 
-                            <Route key={`main-route-${index}`} path={route.path} status={route.status}>
-                                <NotFound />
-                            </Route>;
-                        })
-                    }
-                </Switch>
+                <Suspense
+                    fallback={<Spinner />}>
+                    <Switch>
+                        {data.map((route, index) => {
+                            return route.redirect.behavior && route.exact ? 
+                                <Route 
+                                    key={`main-route-${index}`} 
+                                    path={route.path} 
+                                    exact >
+                                    <Redirect to={route.redirect.path} />
+                                </Route> : route.redirect.behavior && !route.exact ? 
+                                <Route 
+                                    key={`main-route-${index}`} 
+                                    path={route.path} >
+                                    <Redirect to={route.redirect.path} />
+                                </Route> : route.component === "Home" ?
+                                <Route 
+                                    key={`main-route-${index}`} 
+                                    path={route.path} 
+                                    exact >
+                                    <Home />
+                                </Route> : route.component === "Gallery" ?
+                                <Route 
+                                    key={`main-route-${index}`} 
+                                    path={route.path} >
+                                    <Gallery />
+                                </Route> : 
+                                <Route 
+                                    key={`main-route-${index}`} 
+                                    path={route.path} status={route.status}>
+                                    <NotFound />
+                                </Route>;
+                            })
+                        }
+                    </Switch>
+                </Suspense>
             </Card>
         </main>
     );
