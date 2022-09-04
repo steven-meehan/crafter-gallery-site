ac582b7dc9db4b6176909917d3d1de305c4ec37d
Steven P Meehan <steven@stevenmeehan.com>
2022-09-04 18:19:03 -0400

    Updated the useHttp hook to chace web requests

--- src/Hooks/useHttp.tsx
+++ src/Hooks/useHttp.tsx
@@ -1,33 +1,82 @@
 import { useState, useCallback } from 'react';
 
+import CachedItem from '../Models/Hooks/CachedItem';
+
 const useHttp = () => {
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState("");
+    const [error, setError] = useState<string[]>([]);
+
+    const currentTime = () => { return Date.now(); }
 
     const sendRequest = useCallback(async (requestConfig: {
         url: string,
         method?: string,
         headers?: HeadersInit,
-        body?: any
+        body?: any,
+        cacheAge: number
     }, applyData: any) => {
         setIsLoading(true);
-        setError("");
+        setError([]);
+
+        const makeHttpRequest = async () => {
+            try {
+                const response = await fetch(requestConfig.url, {
+                    method: requestConfig.method ? requestConfig.method : 'GET',
+                    headers: requestConfig.headers ? requestConfig.headers : {},
+                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Request failed!');
+                }
+    
+                const data = await response.json();
+
+                writeDataToSessionStorage(data);
+                applyData(data);
+            } catch (err) {
+                setError(prevState => {
+                    return [...prevState, 'Something went wrong with the request. Please try again later.'];
+                });
+            }
+        }
+
+        const writeDataToSessionStorage = (data: any) => {
+            const cachedData = new CachedItem({
+                data: data,
+                timestamp: currentTime() + requestConfig.cacheAge
+            })
+
+            try{
+                sessionStorage.setItem(requestConfig.url, JSON.stringify(cachedData))
+            }
+            catch(e){
+                setError(prevState => {
+                    return [...prevState, `There was a problem setting the data for ${requestConfig.url} in session storage`];
+                });
+            }
+        }
 
         try {
-            const response = await fetch(requestConfig.url, {
-                method: requestConfig.method ? requestConfig.method : 'GET',
-                headers: requestConfig.headers ? requestConfig.headers : {},
-                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-            });
+            const cachedData = sessionStorage.getItem(requestConfig.url)
+    
+            if(cachedData) {
+                const cachedItem = JSON.parse(cachedData) as CachedItem;
 
-            if (!response.ok) {
-                throw new Error('Request failed!');
+                if(cachedItem.timestamp > currentTime()){
+                    applyData(cachedItem.data!);
+                } else {
+                    sessionStorage.removeItem(requestConfig.url);
+                    makeHttpRequest();
+                }
+            } else {
+                makeHttpRequest();
             }
-
-            const data = await response.json();
-            applyData(data);
-        } catch (err) {
-            setError('Something went wrong with the request. Please try again later.');
+        }
+        catch(e){
+            setError(prevState => {
+                return [...prevState, `There was a problem pulling ${requestConfig.url} from session storage`];
+            });
         }
 
         setIsLoading(false);
