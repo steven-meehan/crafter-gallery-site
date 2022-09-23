fb060ef32fcd3319282976248b4dd89adfd0f246
Steven P Meehan <steven@stevenmeehan.com>
2022-09-22 22:32:02 -0400

    implemented the PageRow component

--- src/Components/Content/Page/Page.tsx
+++ src/Components/Content/Page/Page.tsx
@@ -2,13 +2,11 @@ import React, { Fragment, useEffect, useState } from 'react';
 import HelmetSettings from '../../Structure/Helmet/HelmetSettings';
 
 import PageProps from './PageProps';
-import PageProcessor from './PageProcessor';
+import PageProcessor from '../../../Processors/PageProcessor';
 import PageData from '../../../Models/DataFiles/PageData/PageData';
-import PageComponent from '../../../Models/DataFiles/PageData/PageComponent';
 import useHttp from '../../../Hooks/useHttp';
-import ColumnPosition from '../../../Models/DataFiles/PageData/ColumnPosition';
-import NumberOfColumns from '../../../Models/DataFiles/PageData/NumberOfColumns';
 import Spinner from '../../UI/Spinner/Spinner';
+import PageRow from '../../UI/Layouts/PageRow';
 
 import config from '../../../ConfigurationFiles/data-file-locations.json';
 import seoConfig from '../../../ConfigurationFiles/seo-config.json';
@@ -21,15 +19,25 @@ const Page: React.FC<PageProps> = (props) => {
     const pathname = window.location.pathname;
 
     const { sendRequest: fetchParagraphs, isLoading } = useHttp();
-    const [ components, setComponents ] = useState<PageComponent[]>([]);
-    const [ columns, setColumns ] = useState<number>(1);
+    const [ pageRows, setPageRows ] = useState<JSX.Element[]>([]);
     const [ header, setHeader ] = useState<string>("");
 
-    useEffect(() => {       
+    useEffect(() => {
+        setPageRows([]);
+        setHeader("");
         const transformData = (data: PageData) =>{
             setHeader(data.header);
-            setColumns(data.layout.columns.numberOfColumns);
-            setComponents(PageProcessor.PrepareDataFile(data));
+            const pageComponents = PageProcessor.PrepareDataFile(data);
+            const contentRows = data.layout.rows.map((row, index) :JSX.Element => {
+                return (
+                    <PageRow 
+                        key={`${data.name}-page-row-${row.order}`}
+                        pageRow={row} 
+                        pageComponents={pageComponents.filter(item=>item.componentRow === index)} />
+                )
+            });
+
+            setPageRows(contentRows);
         }
 
         fetchParagraphs(
@@ -39,7 +47,7 @@ const Page: React.FC<PageProps> = (props) => {
             },
             transformData
         );
-    }, [fetchParagraphs, pathname]);
+    }, [fetchParagraphs, pathname, dataFile.cacheAge, dataFile.url]);
 
     return (
         <Fragment>
@@ -47,45 +55,17 @@ const Page: React.FC<PageProps> = (props) => {
                 helmetConfiguration={seoInfo} 
                 seoSiteUrl={seoConfig.site} />
             {isLoading && <Spinner />}
-            {components && !isLoading && (
-                columns === NumberOfColumns.Two ? (
-                    <div className={`row`} >
-                        <div className={`col`} >
-                            <div className={`row`} >
+            {(pageRows && !isLoading) && (
+                <div className={`row`} >
+                    <div className={`col`}>
+                        <div className={`row`}>
+                            <div className={`col`}>
                                 <h1>{header}</h1>
                             </div>
-                            <div className={`row`} >
-                                <div className={`col-xs-12 col-xl-${12/columns}`}>
-                                    {
-                                        components
-                                            .map(item=> {
-                                                if(item.componentPosition===ColumnPosition.Left){
-                                                    return item.jsxElement
-                                                }
-                                            })
-                                    }
-                                </div>
-                                <div className={`col-xs-12 col-xl-${12/columns}`}>
-                                    {
-                                        components
-                                        .map(item=> {
-                                            if(item.componentPosition===ColumnPosition.Right){
-                                                return item.jsxElement
-                                            }
-                                        })
-                                    }
-                                </div>
-                            </div>
                         </div>
+                        {pageRows}
                     </div>
-                ) : columns === NumberOfColumns.One ? (
-                    <div className={`row`} >
-                        <div className={`col-xs-12 col-xl-${12/columns}`}>
-                            <h1>{header}</h1>
-                            {components.map(item=>item.jsxElement)}
-                        </div>
-                    </div>
-                ) : (<></>)
+                </div>
             )}
         </Fragment>
     );
